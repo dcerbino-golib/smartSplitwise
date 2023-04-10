6c011388238ca68236437c7a5ff987c850726245
David Cerbino <dcerbino@gmail.com>
2023-04-10 10:09:29 -0300

    first commit

---
+++ splitwise.go
@@ -0,0 +1,320 @@
+package smartsplitwise
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aanzolaavila/splitwise.go"
+	"github.com/aanzolaavila/splitwise.go/resources"
+)
+
+type splitwiseResouces interface {
+	resources.Expense |
+		resources.MainCategory |
+		resources.Currency |
+		resources.Friend |
+		resources.Group |
+		resources.Notification
+}
+
+type swConnectionStruct struct {
+	ctx    context.Context
+	client splitwise.Client
+}
+
+type SwConnection interface {
+	GetMainCategories() CommandExecutor[resources.MainCategory]
+	GetMainCategory(id resources.Identifier) (*resources.MainCategory, error)
+	GetCurecies() CommandExecutor[resources.Currency]
+	GetCurency(code string) (*resources.Currency, error)
+	GetFriends() CommandExecutor[resources.Friend]
+	GetFriend(id int) (resources.Friend, error)
+	GetGroups() CommandExecutor[resources.Group]
+	GetGroup(id int) (resources.Group, error)
+	GetNotifications(params splitwise.NotificationsParams) CommandExecutor[resources.Notification]
+	GetExpense(id int) (resources.Expense, error)
+	GetExpenses(params splitwise.ExpensesParams) CommandExecutor[resources.Expense]
+	getClient() splitwise.Client
+	getCtx() context.Context
+}
+
+type commandExecutorStruct[T splitwiseResouces] struct {
+	SwConnection
+	ch    chan T
+	close bool
+}
+
+type CommandExecutor[T splitwiseResouces] interface {
+	isClose() bool
+	Close()
+	GetChan() <-chan T
+}
+
+type ElementNotFound struct{}
+
+func (m *ElementNotFound) Error() string {
+	return "Element Not Found"
+}
+
+func getTokenClient(token string) splitwise.Client {
+	return splitwise.Client{
+		Token: token,
+	}
+}
+
+var mainCategoryCache map[resources.Identifier]resources.MainCategory = make(map[resources.Identifier]resources.MainCategory)
+var curenciesCache map[string]resources.Currency = make(map[string]resources.Currency)
+
+func Open(token string, ctx context.Context, log *log.Logger) SwConnection {
+
+	conn := &swConnectionStruct{}
+
+	conn.client = getTokenClient(token)
+	conn.client.Logger = log
+	conn.ctx = ctx
+	return conn
+}
+
+func (cs *swConnectionStruct) getCtx() context.Context {
+	return cs.ctx
+}
+
+func (ce *commandExecutorStruct[T]) isClose() bool {
+	return ce.close
+}
+
+func (ce *commandExecutorStruct[T]) Close() {
+	ce.close = true
+	for range ce.ch {
+	}
+}
+
+func (ce *commandExecutorStruct[T]) GetChan() <-chan T {
+	return ce.ch
+}
+
+func simpleExecutor[T splitwiseResouces](conn SwConnection, method func(ctx context.Context) ([]T, error)) CommandExecutor[T] {
+	ch := make(chan T)
+	ce := commandExecutorStruct[T]{}
+	ce.ch = ch
+	ce.SwConnection = conn
+	ce.close = false
+
+	go func(ch chan<- T) {
+		defer ce.cleanCe()
+		defer recoverClosedChannel()
+		entities, err := method(ce.getCtx())
+
+		if err != nil {
+			ce.getClient().Logger.Printf(err.Error())
+			return
+		}
+
+		for _, e := range entities {
+			if ce.isClose() {
+				return
+			}
+			ch <- e
+		}
+
+	}(ch)
+	return &ce
+
+}
+
+func (conn *swConnectionStruct) GetMainCategory(id resources.Identifier) (*resources.MainCategory, error) {
+	result, ok := mainCategoryCache[id]
+	if !ok {
+		return nil, &ElementNotFound{}
+	}
+
+	return &result, nil
+}
+
+func (conn *swConnectionStruct) GetMainCategories() CommandExecutor[resources.MainCategory] {
+	ch := make(chan resources.MainCategory)
+	ce := commandExecutorStruct[resources.MainCategory]{}
+	ce.ch = ch
+	ce.SwConnection = conn
+	ce.close = false
+
+	go func(ch chan<- resources.MainCategory) {
+		defer ce.cleanCe()
+		defer recoverClosedChannel()
+		for _, v := range mainCategoryCache {
+			ch <- v
+		}
+	}(ch)
+	return &ce
+}
+
+func (conn *swConnectionStruct) GetCurency(code string) (*resources.Currency, error) {
+	result, ok := curenciesCache[code]
+	if !ok {
+		return nil, &ElementNotFound{}
+	}
+
+	return &result, nil
+}
+
+func (conn *swConnectionStruct) GetCurecies() CommandExecutor[resources.Currency] {
+	ch := make(chan resources.Currency)
+	ce := commandExecutorStruct[resources.Currency]{}
+	ce.ch = ch
+	ce.SwConnection = conn
+	ce.close = false
+
+	go func(ch chan<- resources.Currency) {
+		defer ce.cleanCe()
+		defer recoverClosedChannel()
+		for _, v := range curenciesCache {
+			ch <- v
+		}
+	}(ch)
+	return &ce
+
+}
+
+func (conn *swConnectionStruct) GetFriends() CommandExecutor[resources.Friend] {
+	client := conn.getClient()
+	return simpleExecutor(conn, client.GetFriends)
+}
+
+func (conn *swConnectionStruct) GetFriend(id int) (resources.Friend, error) {
+	client := conn.getClient()
+	return client.GetFriend(conn.ctx, id)
+}
+
+func (conn *swConnectionStruct) GetGroups() CommandExecutor[resources.Group] {
+	client := conn.getClient()
+
+	return simpleExecutor(conn, client.GetGroups)
+}
+
+func (conn *swConnectionStruct) GetGroup(id int) (resources.Group, error) {
+	client := conn.getClient()
+
+	return client.GetGroup(conn.ctx, id)
+}
+
+func (conn *swConnectionStruct) GetNotifications(params splitwise.NotificationsParams) CommandExecutor[resources.Notification] {
+	ch := make(chan resources.Notification)
+	ce := commandExecutorStruct[resources.Notification]{}
+	ce.ch = ch
+	ce.SwConnection = conn
+	ce.close = false
+
+	go func(ch chan<- resources.Notification) {
+		defer ce.cleanCe()
+		defer recoverClosedChannel()
+		client := conn.getClient()
+
+		notifications, err := client.GetNotifications(ce.getCtx(), params)
+		if err != nil {
+			ce.getClient().Logger.Printf(err.Error())
+			return
+		}
+
+		for _, e := range notifications {
+			if ce.isClose() {
+				return
+			}
+			ch <- e
+		}
+
+	}(ch)
+
+	return &ce
+}
+
+func (conn *swConnectionStruct) GetExpense(id int) (resources.Expense, error) {
+	client := conn.getClient()
+
+	return client.GetExpense(conn.ctx, id)
+}
+
+func (conn *swConnectionStruct) GetExpenses(params splitwise.ExpensesParams) CommandExecutor[resources.Expense] {
+	ch := make(chan resources.Expense)
+	ce := commandExecutorStruct[resources.Expense]{}
+
+	ce.ch = ch
+	ce.SwConnection = conn
+	ce.close = false
+
+	go func(ch chan<- resources.Expense) {
+		defer ce.cleanCe()
+		defer recoverClosedChannel()
+		client := conn.getClient()
+
+		var (
+			cont int
+		)
+		for !ce.close {
+			expenses, err := client.GetExpenses(ce.getCtx(), params)
+			if err != nil {
+				ce.getClient().Logger.Printf(err.Error())
+				break
+			}
+
+			if len(expenses) == 0 {
+				break
+			}
+
+			for _, e := range expenses {
+				if ce.isClose() {
+					return
+				}
+				ch <- e
+				cont++
+			}
+			incOffset(params, cont)
+		}
+		fmt.Println("Exit background function")
+	}(ch)
+
+	return &ce
+}
+
+func (conn *swConnectionStruct) getClient() splitwise.Client {
+	return conn.client
+}
+
+func incOffset(params splitwise.ExpensesParams, inc int) {
+	params[splitwise.ExpensesOffset] = inc
+}
+
+func recoverClosedChannel() {
+	// recover from panic caused by writing to a closed channel
+	if r := recover(); r != nil {
+		err := fmt.Errorf("%v", r)
+		fmt.Printf("write: error writing on channel: %v\n", err)
+		return
+	}
+}
+
+func (ce *commandExecutorStruct[T]) cleanCe() {
+	ce.close = true
+	close(ce.ch)
+}
+
+func init() {
+	conn := Open("", context.Background(), log.New(os.Stdout, "Splitwise Init LOG: ", log.Lshortfile))
+
+	client := conn.getClient()
+
+	ceCurrencies := simpleExecutor(conn, client.GetCurrencies)
+	for v := range ceCurrencies.GetChan() {
+		curenciesCache[v.CurrencyCode] = v
+	}
+
+	ceMainCategory := simpleExecutor(conn, client.GetCategories)
+
+	for v := range ceMainCategory.GetChan() {
+		mainCategoryCache[resources.Identifier(v.ID)] = v
+	}
+
+	fmt.Printf("Currency cache loaded with %d values\n", len(curenciesCache))
+	fmt.Printf("Main Category cache loaded with %d values\n", len(mainCategoryCache))
+}
---
+++ splitwise_test.go
@@ -0,0 +1,315 @@
+package smartsplitwise
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/aanzolaavila/splitwise.go/resources"
+	"github.com/stretchr/testify/assert"
+)
+
+const getFriends200Response = `
+{
+  "friends": [
+    {
+      "id": 15,
+      "first_name": "Ada",
+      "last_name": "Lovelace",
+      "email": "ada@example.com",
+      "registration_status": "confirmed",
+      "picture": {
+        "small": "string",
+        "medium": "string",
+        "large": "string"
+      },
+      "groups": [
+        {
+          "group_id": 571,
+          "balance": [
+            {
+              "currency_code": "USD",
+              "amount": "414.5"
+            }
+          ]
+        }
+      ],
+      "balance": [
+        {
+          "currency_code": "USD",
+          "amount": "414.5"
+        }
+      ],
+      "updated_at": "2019-08-24T14:15:22Z"
+    },
+	{
+		"id": 16,
+		"first_name": "Pepe",
+		"last_name": "ponce",
+		"email": "pepe@example.com",
+		"registration_status": "confirmed",
+		"picture": {
+		  "small": "string",
+		  "medium": "string",
+		  "large": "string"
+		},
+		"groups": [
+		  {
+			"group_id": 571,
+			"balance": [
+			  {
+				"currency_code": "USD",
+				"amount": "329.5"
+			  }
+			]
+		  }
+		],
+		"balance": [
+		  {
+			"currency_code": "USD",
+			"amount": "329.5"
+		  }
+		],
+		"updated_at": "2019-08-23T14:15:22Z"
+	  }
+  ]
+}
+`
+
+type httpClientStub struct {
+	DoFunc func(*http.Request) (*http.Response, error)
+}
+
+func (c httpClientStub) Do(r *http.Request) (*http.Response, error) {
+	if c.DoFunc == nil {
+		panic("mocked function is nil")
+	}
+
+	return c.DoFunc(r)
+}
+
+type logger interface {
+	Printf(string, ...interface{})
+}
+
+type testLogger struct {
+	buf    bytes.Buffer
+	logger logger
+	once   sync.Once
+	T      *testing.T
+}
+
+func (l testLogger) Printf(s string, args ...interface{}) {
+	l.once.Do(func() {
+		tname := l.T.Name()
+		prefix := fmt.Sprintf("%s:: ", tname)
+		l.logger = log.New(io.Writer(&l.buf), prefix, log.LstdFlags)
+
+		l.T.Cleanup(func() {
+			if l.T.Failed() {
+				fmt.Print(l.buf.String())
+			}
+		})
+	})
+
+	l.logger.Printf(s, args...)
+}
+
+func TestOpen(t *testing.T) {
+	assert := assert.New(t)
+
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	result := Open(token, ctx, log)
+
+	assert.Equal(token, result.getClient().Token)
+	assert.Equal(ctx, result.getCtx())
+	assert.Equal(log, result.getClient().Logger)
+}
+
+func TestMainCategoryCache(t *testing.T) {
+	assert.Equal(t, true, len(mainCategoryCache) >= 7)
+}
+
+func TestCurenciesCache(t *testing.T) {
+	assert.Equal(t, true, len(curenciesCache) >= 148)
+}
+
+func TestClose(t *testing.T) {
+	assert := assert.New(t)
+
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	executor := conn.GetCurecies()
+
+	assert.Equal(false, executor.isClose())
+	executor.Close()
+	assert.Equal(true, executor.isClose())
+}
+
+func TestGetCategory(t *testing.T) {
+	assert := assert.New(t)
+
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	category, err := conn.GetMainCategory(resources.Identifier(1))
+	assert.Equal(nil, err)
+	assert.Equal("Utilities", category.Name)
+
+}
+
+func TestGetCategoryNotFound(t *testing.T) {
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	_, err := conn.GetMainCategory(resources.Identifier(0))
+	assert.EqualErrorf(t, err, (&ElementNotFound{}).Error(), "Error should be: %v, got: %v", (&ElementNotFound{}).Error(), err)
+
+}
+
+func TestGetCategoryies(t *testing.T) {
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	executor := conn.GetMainCategories()
+	cont := 0
+	want := 7
+
+	for range executor.GetChan() {
+		cont++
+	}
+
+	assert.GreaterOrEqual(t, cont, want, "Get categories should return unless %d arg and got it %d", want, cont)
+}
+
+func TestGetCurrencies(t *testing.T) {
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	executor := conn.GetCurecies()
+	cont := 0
+	want := 148
+
+	for range executor.GetChan() {
+		cont++
+	}
+
+	assert.GreaterOrEqual(t, cont, want, "Get currencies should return unless %d currencies and got it %d", want, cont)
+}
+
+func TestGetCurrency(t *testing.T) {
+	assert := assert.New(t)
+
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	currencyCode := "USD"
+	currencyUnit := "$"
+
+	currency, err := conn.GetCurency(currencyCode)
+
+	assert.NoError(err, "%s should be present as a currency code", currencyCode)
+	assert.Equal(currencyUnit, currency.Unit, "%s currency unit should be %s but got %s", currencyCode, currencyUnit, currency.Unit)
+}
+
+func TestGetCurrencyNotFund(t *testing.T) {
+	assert := assert.New(t)
+
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	currencyCode := "US"
+
+	_, err := conn.GetCurency(currencyCode)
+
+	assert.EqualErrorf(err, (&ElementNotFound{}).Error(), "Error should be: %v, got: %v", (&ElementNotFound{}).Error(), err)
+}
+
+func TestGetFriends(t *testing.T) {
+	doFunc := func(r *http.Request) (*http.Response, error) {
+		resposne := http.Response{}
+		resposne.Body = io.NopCloser(strings.NewReader(getFriends200Response))
+		resposne.Header = make(map[string][]string)
+		resposne.Header["Content-Type"] = []string{"application/json", "charset=utf-8"}
+		resposne.Status = "200"
+		resposne.StatusCode = 200
+		return &resposne, nil
+	}
+
+	type responseStruct struct {
+		Friends []resources.Friend
+	}
+	wantedRespounce := responseStruct{}
+
+	err := json.Unmarshal([]byte(getFriends200Response), &wantedRespounce)
+
+	if err != nil {
+		panic(err)
+	}
+
+	conn := getTestConnection(t, doFunc)
+
+	executor := conn.GetFriends()
+
+	cont := 0
+
+	for range executor.GetChan() {
+		cont++
+	}
+
+	assert.Equal(t, len(wantedRespounce.Friends), cont)
+}
+
+func getTestConnection(t *testing.T, doFunc func(r *http.Request) (*http.Response, error)) SwConnection {
+	client := Open("testtoken", context.Background(), log.New(os.Stdout, "Test Splitwise LOG: ", log.Lshortfile))
+
+	bareclient, ok := client.(*swConnectionStruct)
+
+	if !ok {
+		panic("unable to convert interface to istance")
+	}
+
+	cliststub := httpClientStub{
+		DoFunc: doFunc,
+	}
+
+	bareclient.client.HttpClient = cliststub
+	bareclient.client.Logger = testLogger{
+		T: t,
+	}
+
+	return bareclient
+}
